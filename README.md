b8fdde5a5ceb49b24f4a262d0be540b075161da2
n0d4wn - myven _Chris_B_ <chris.j.bruhin@gmail.com>
2024-10-27 19:21:53 +0100

    initial design human
    added files

---
+++ README.md
@@ -0,0 +1,2 @@
+# 00_ai_audiotool_html5
+This is the initial setup of the ai (copilot Microsoft)
---
+++ app/player/script.js
@@ -0,0 +1,15 @@
+function playTrack1() {
+    document.getElementById('track1').play();
+}
+
+function pauseTrack1() {
+    document.getElementById('track1').pause();
+}
+
+function playTrack2() {
+    document.getElementById('track2').play();
+}
+
+function pauseTrack2() {
+    document.getElementById('track2').pause();
+}
